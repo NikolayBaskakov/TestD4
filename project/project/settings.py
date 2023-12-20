f9083db97fbedb3c4ca94a48d76c12f5a8f984fc
NikolayBaskakov <bnn300702@yandex.ru>
2023-12-20 08:59:09 +0300

    g

--- project/project/settings.py
+++ project/project/settings.py
@@ -146,3 +146,13 @@ ACCOUNT_USERNAME_REQUIRED = False
 ACCOUNT_AUTHENTICATION_METHOD = 'email'
 ACCOUNT_EMAIL_VERIFICATION = 'none'
 ACCOUNT_FORMS = {"signup": "accounts.forms.CustomSignupForm"}
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.yandex.ru'
+EMAIL_PORT = 465
+EMAIL_HOST_USER = "n.n.baskakov"
+EMAIL_HOST_PASSWORD = "bikoquulvwfhcbwz"
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
+
+DEFAULT_FROM_EMAIL = "n.n.baskakov@yandex.ru"
